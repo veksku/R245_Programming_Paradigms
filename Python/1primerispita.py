54c8ada6afb7df88557e44ee9f56ea852a8eae4a
Veljko Kučinar <57194423+veksku@users.noreply.github.com>
2021-06-08 23:52:52 +0200

    Add files via upload

---
+++ Python/1primerispita.py
@@ -0,0 +1,38 @@
+import sys, json
+
+if len(sys.argv) != 2:
+	print("Nisu navedeni argumenti komandne linije kako treba")
+	exit()
+
+path = sys.argv[1]
+
+try:
+	with open(path, "r") as f:
+		igraci = json.load(f)
+except IOError:
+	print("Otvaranje datoteke nije uspelo!")
+	sys.exit()
+	
+input_godina = input("Unesite gornju granicu starosti igraca: ")
+input_pozicija = input("Unesite poziciju igraca: ")
+
+max_visina = -1
+
+for igrac in igraci:
+	visina = igrac['visina']
+	godine = igrac['godine']
+#	print(igrac['ime'] + " " + str(visina) + " " + str(godine))
+	pozicija = -1
+	if visina < 190:
+		pozicija = 1
+	elif visina >= 200:
+		pozicija = 5
+	else:
+		pozicija = 3
+#	print("Pozicija: " + str(pozicija))
+	if godine <= int(input_godina):
+		if visina > max_visina:
+			if pozicija == int(input_pozicija):
+				max_visina = visina
+			
+print(max_visina)
