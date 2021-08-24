b01fe5e203164d8553d801aec870e615b6834aa6
Veljko Kučinar <57194423+veksku@users.noreply.github.com>
2021-08-24 18:50:04 +0200

    Add files via upload

---
+++ Haskell/3primerispita.hs
@@ -0,0 +1,21 @@
+otvori :: [(String, Int)] -> String -> [(String, Int)]
+zatvori :: [(String, Int)] -> String -> [(String, Int)]
+uplati :: [(String, Int)] -> String -> Int -> [(String, Int)]
+
+
+otvori b br
+	| (filter (\(x,y) -> x == br) b) /= [] = b
+	| otherwise = (br, 0) : b
+
+{-
+zatvori b br
+	| filter (\(x,y) -> x == br) b == [] = b
+	| otherwise = takeWhile (\(x,y) -> x /= br) b ++ tail(drugideo)
+		where drugideo = dropWhile (\(x,y) -> x /= br) b
+-}
+
+zatvori [] _ = []
+zatvori (x:xs) br = if br == (fst x) then xs else x : zatvori xs br
+
+uplati (x:xs) br iznos = if br == (fst x) then (fst x, (snd x) + iznos) : xs else x : uplati xs br iznos
+
