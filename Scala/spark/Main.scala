d62dbd013429ca3505dcd795269ca28c1c90745b
Veljko Kučinar <57194423+veksku@users.noreply.github.com>
2022-01-13 03:57:33 +0100

    Add files via upload

---
+++ Scala/spark/Main.scala
@@ -0,0 +1,47 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+    val conf : SparkConf = new SparkConf()
+      .setAppName("MainSparkApp")
+      .setMaster("local[4]")
+
+    val context = new SparkContext(conf)
+    val rdd = context.textFile("cars.csv")
+      .filter(linija => linija.contains("AWD") && linija.contains("Gasoline") && linija.contains("Automatic"))
+      .filter(linija => (linija.contains("5 Speed Automatic") || linija.contains("6 Speed Automatic")))
+      .map(linija => {
+        val niz = linija.split(",")
+        (niz(3).trim().toFloat, (niz(7).trim().toInt,  niz(8).trim().toInt))
+        //mpg                   godina                snaga
+      })
+      .filter(linija => linija._2._1 > 2010 && linija._2._2 > 300)
+      .map(r => ("AWD", r._1))
+      .aggregateByKey((0.0, 0))((acc, v) => (acc._1 + v, acc._2 + 1),
+        (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
+      .map(r => r._2._1 / r._2._2)
+      .foreach(r => println(r))
+    //    val cache = context.textFile("cars.csv")
+//      .filter(linija => linija.contains("AWD") && linija.contains("Gasoline") && linija.contains("Automatic"))
+//      .filter(linija => (linija.contains("5 Speed Automatic") || linija.contains("6 Speed Automatic")))
+//      .map(linija => {
+//        val niz = linija.split(",")
+//        (niz(3).trim().toFloat, niz(7).trim().toInt,  niz(8).trim().toInt)
+//        //mpg                   godina                snaga
+//      })
+//      .filter(linija => linija._2 > 2010 && linija._3 > 300)
+//      .cache()
+//
+//    val brojLinija = cache
+//        .count()
+//
+//    val sumMPG = cache
+//        .map(r => r._1)
+//        .reduce(_+_)
+//
+//    println(sumMPG/brojLinija.toFloat)
+    context.stop()
+  }
+}
