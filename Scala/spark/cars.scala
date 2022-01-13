c2c92a3345ae9808d2eee87e93b0a4b829f52a59
Veljko Kučinar <57194423+veksku@users.noreply.github.com>
2022-01-13 04:06:54 +0100

    Update cars.scala

--- Scala/spark/cars.scala
+++ Scala/spark/cars.scala
@@ -10,18 +10,19 @@ object cars {
 
     val context = new SparkContext(conf)
     val rdd = context.textFile("cars.csv")
-      .filter(linija => linija.contains("AWD") && linija.contains("Gasoline") && linija.contains("Automatic"))
+      .filter(linija => linija.startsWith("AWD") && linija.contains("Gasoline") && linija.contains("Automatic"))
       .filter(linija => (linija.contains("5 Speed Automatic") || linija.contains("6 Speed Automatic")))
       .map(linija => {
         val niz = linija.split(",")
-        (niz(3).trim().toFloat, (niz(7).trim().toInt,  niz(8).trim().toInt))
-        //mpg                   godina                snaga
+        (niz(0).trim(), niz(3).trim().toFloat, niz(7).trim().toInt,  niz(8).trim().toInt)
+        //pogon         mpg                    godina                snaga
       })
-      .filter(linija => linija._2._1 > 2010 && linija._2._2 > 300)
-      .map(r => ("AWD", r._1))
-      .aggregateByKey((0.0, 0))((acc, v) => (acc._1 + v, acc._2 + 1),
+      .filter(linija => linija._3 > 2010 && linija._4 > 300)
+      .map(r => (r._1, r._2)) //sklanjamo godinu i snagu jer smo isfilterisali one koji nam trebaju, pravimo kljuc i vrednost
+      .aggregateByKey((0.0, 0))((acc, v) => (acc._1 + v, acc._2 + 1), 
+      //za svaki kljuc (0,0, 0) => "0,0" je cesto zbir, "0" je cesto brojac; v je jedna od vrednosti iz proslog mapiranja (nikako kljuc)
         (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
-      .map(r => r._2._1 / r._2._2)
+      .map(r => r._2._1 / r._2._2) //oblika je (kljuc, (zbir, brojac))
       .foreach(r => println(r))
     //    val cache = context.textFile("cars.csv")
 //      .filter(linija => linija.contains("AWD") && linija.contains("Gasoline") && linija.contains("Automatic"))
